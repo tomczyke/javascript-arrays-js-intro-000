2e80c90457344861587460734ecb34a2f457388b
Evan Tomczyk <tomczykej@gmail.com>
2019-06-26 08:21:41 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -15,7 +15,7 @@ function destructivelyAddElementToBeginningOfArray(array,element) {
 }
 
 function addElementToEndOfArray(array,element) {
-  return [array, ...element];
+  return [...array, element];
   }
 
 function destructivelyAddElementToEndOfArray(array,element) {
