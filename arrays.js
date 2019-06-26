f11ef90492590932d6930a2b4bb0e16857bf5306
Evan Tomczyk <tomczykej@gmail.com>
2019-06-26 08:29:40 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -33,6 +33,5 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1);
-  return array
+  return array.slice(1);
 }
