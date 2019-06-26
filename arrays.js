0e05520c1bce56214193f8d59160d0159dfbef15
Evan Tomczyk <tomczykej@gmail.com>
2019-06-26 08:24:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -27,6 +27,6 @@ function accessElementInArray(array,index) {
     return array[index];
 }
 
-function destructivelyRemoveElementFromEndOfArray(array) {
+function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array.shift();
 }
